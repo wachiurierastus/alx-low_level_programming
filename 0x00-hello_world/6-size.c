d2c784cfc8c5f365fac067997b69956cab124e54
andrewnabaala <andrew.k.nabaala@gmail.com>
2022-03-10 12:25:57 -0800

    Getting better in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+int main(void)
+{
+
+	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an double: %lu byte(s)\n", sizeof(double));
@@ -8,4 +20,5 @@ int main(void){
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
+
 }
