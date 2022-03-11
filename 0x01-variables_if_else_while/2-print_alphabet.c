426d0f4ac7d90504ae95ca0602c5fe959f2bcbe4
andrewnabaala <andrew.k.nabaala@gmail.com>
2022-03-11 13:50:17 -0800

    better

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * Main -entry point/.
+ *
+ * Description - prints"abcdefghijklmnopqrstuvwxyz"
+ *
+ * Return (0);
+ */
+int main(){
+	char a;
+	for (a = 'a' ; a <= 'z' ; a++){
+		putchar(a)
+	}
+	return (0);
+}
