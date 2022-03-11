18d1c7bb237b1a37adff78fd6a64dce30ed59500
wachiurierastus <wachiurierastus@github.com>
2022-03-11 14:09:59 +0000

    Task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* main- Entry point of main method
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n <0)
+	{
+		puts(n " is negative");
+	}
+	else if (n == 0)
+	{
+		puts(n " is zero");
+	}
+	else
+		puts(n " is positive");
+
+	return (0);
+}
