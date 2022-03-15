6c5351e844e9af4887777d56cbca9dee0200739e
wachiurierastus <wachiurierastus@github.com>
2022-03-15 17:49:33 +0000

    Trials

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,22 @@
 #include "main.h"
 /**
- * jack_bauer - to print all minutes from 00 to 2459
+ *jack_bauer -  count down 24hrs
  *
- * Return: nothing
+ *Return: void.
  */
-void jack_bauer(void)
+void  jack_bauer(void)
 {
-	int i;
-
-	for (i = 0; i <= 2; i++)
+	int hour, minute;
+	for (hour = 0; hour <= 23; hour++)
 	{
-		int j;
-
-		for (i = 0; i < 10; i++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			int k;
-
-			for (k = 0; k < 60; k++)
-			{
-				_putchar(i);
-				_putchar(j);
-				_putchar(':');
-				if (k < 10)
-				{	_putchar('0');
-					_putchar(k);
-				}
-				else
-					_putchar(k);
-			}
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+		_putchar('\n');
 		}
 	}
 }
