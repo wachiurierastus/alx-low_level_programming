c679cd675e10e466aa1ea6eb4d3fbdcc91831bff
wachiurierastus <wachiurierastus@github.com>
2022-03-15 13:45:57 +0000

    Trials

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -7,15 +8,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i;
+	char p[] ="_putchar";
+	for(i = 0;i < 9; i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
 	return (0);
 
 }
