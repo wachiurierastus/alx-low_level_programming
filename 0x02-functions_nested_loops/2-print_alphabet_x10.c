34cff4721de28c0575acb7233324cf3e8cec38be
wachiurierastus <wachiurierastus@github.com>
2022-03-15 14:22:14 +0000

    Task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all characters
+ *Description: prints all lowercase char
+ *
+ * Return : void
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i =0; i<10; i++)
+	{
+		char ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n\);
+	}
+}
