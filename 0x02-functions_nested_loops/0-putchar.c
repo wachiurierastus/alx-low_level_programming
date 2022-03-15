e20535e9578507f3fd118429e4caf21741259ba8
andrewnabaala <andrew.k.nabaala@gmail.com>
2022-03-15 15:42:02 -0700

    better

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: o - Success
+ * prints _putchar plus a new line
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
+	return (0);
+
+}
