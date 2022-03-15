d3302bc62c9f15458d260eced562206c70d2b5c0
wachiurierastus <wachiurierastus@github.com>
2022-03-15 16:44:51 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - gets absolute value
+ * @n: input
+ *
+ * Return: the abs value of input passed
+ */
+
+int _abs(int n)
+{
+	int j;
+
+	if (n < 0)
+	{
+		j = n * -1;
+	}
+	else
+		j = n;
+}
