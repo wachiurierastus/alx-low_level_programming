a7cc1878bc9abffa0a065ee1ae35bdfb17697fdd
wachiurierastus <wachiurierastus@github.com>
2022-03-15 18:33:05 +0000

    Trials

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 /**
- *main - print first 50 fibonacci
+ *main - print first 98 fibonacci
  *
  *Return: 0 always.
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0;
+	unsigned long int a = 1, b = 2, c, k, p, l, o, m, u, d, i;
 
-	for (i = 0; i < 97; i++)
+	c = a + b;
+	printf("%lu, %lu, ", a, b);
+	d = 3;
+	while (d < 89)
 	{
-		printf("%lu, ", x);
-		sum = x + y;
-		x = y;
-		y = sum;
-		if (i == 96)
-			printf("%lu\n", x);
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
+		d++;
 	}
+	l = b / 1000000000;
+	o = b % 1000000000;
+	m = c / 1000000000;
+	u = c % 1000000000;
+	for (i = 89; i < 98; i++)
+	{
+		printf("%lu%lu, ", m, u);
+		k = l;
+		p = o;
+		l = m;
+		o = u;
+		m = k + l + ((p + o) / 1000000000);
+		u = (p + o) % 1000000000;
+	}
+	printf("%lu%lu\n", m, u);
 	return (0);
 }
