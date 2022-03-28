e7451ac30479c187d3b0e64070916bcd0f4b20ac
andrewnabaala <andrew.k.nabaala@gmail.com>
2022-03-28 04:52:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that uses with put function
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
